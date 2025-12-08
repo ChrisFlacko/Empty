005021a2b1dfe54619f0fa94da276f2e2b09cf57
ChrisFlacko <avanchrisi@gmail.com>
2025-12-08 22:38:02 +0100

    Refinement of the code

--- src/gpio_driver.c
+++ src/gpio_driver.c
@@ -1,9 +1,23 @@
 #include "../include/gpio_driver.h"
+#include "../include/rcc_driver.h"
 
 void Driver_GPIO_SetupPin(void)
 {
     GPIOA->MODER |= GPIO_MODER_SET(5, 1);
     GPIOA->OSPEEDR |= GPIO_OPSPEED_SET(5,1);
-    GPIOA->BSRR = GPIO_BSSR_SET(5,1);
 }
 
+void Driver_GPIO_SetupLED(void)
+{
+    // Enable the clock for GPIOA
+    RCC_ENABLE_GPIOA();
+
+    // Setup the GPIO pin for the LED
+    Driver_GPIO_SetupPin();
+}
+
+void Driver_GPIO_ActivateLED(void)
+{
+    // Set the GPIO pin high to turn on the LED
+    GPIOA->BSRR = GPIO_BSSR_SET(5, 1);
+}
